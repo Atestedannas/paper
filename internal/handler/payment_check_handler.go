f23a31198520bb4029774417edaff05fc0d9473b
李易安 <2673078804@qq.com>
2026-07-15 21:12:40 +0800

    支付策略修改

--- internal/handler/payment_check_handler.go
+++ internal/handler/payment_check_handler.go
@@ -56,6 +56,7 @@ func paperServicePrice(config map[string]interface{}, serviceType string) float6
 	isCheckFree, _ := config["is_check_free"].(bool)
 	if isCheckFree {
 		formatCheck = 0
+		return formatCheck
 	}
 
 	switch serviceType {
